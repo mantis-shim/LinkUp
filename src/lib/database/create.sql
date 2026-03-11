dd1e8193ef79134936005108a12d8cf3ebce4e7a
Mantas Šimaitis <mantas.simaitis@stud.vilniustech.lt>
2026-03-11 17:57:44 +0200

    Created at for users

--- src/lib/database/create.sql
+++ src/lib/database/create.sql
@@ -1,7 +1,8 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE sessions (
