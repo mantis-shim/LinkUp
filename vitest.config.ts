9dc3ce4a7842bbac0798a76f90af71e1a2f48597
Mantas Šimaitis <mantassimaitis37@gmail.com>
2026-04-06 17:57:55 +0300

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,16 @@
-
 import { defineConfig } from 'vitest/config';
 import path from 'path';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   resolve: {
     alias: {
-      $lib: path.resolve(__dirname, './src/lib')
+      $lib: path.resolve('./src/lib')
     }
   },
   test: {
-    environment: 'node'
+    environment: 'node',
+    globals: true,
+    includeSource: ['src/**/*.{ts,js,svelte}']
   }
 });
